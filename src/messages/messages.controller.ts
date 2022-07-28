462478da3dff82085f277151c5e612752510767a
Israel Andrade DEV <israeltandrade@hotmail.com>
2022-07-27 21:13:27 -0300

    passing argument decorators

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
+// O decorator "Controller" é um decorator de classe.
 @Controller('messages')
 export class MessagesController {
   // Rota para listar todas as mensagens:
+  // Aqui temos decorators de método:
   @Get()
   listMessages() {}
 
   // Rota para listar criar uma mensagem:
+  // Decorators de argumento entram nos parâmetros da função.
   @Post()
-  createMessage() {}
+  createMessage(@Body() body: any) {
+    console.log(body);
+  }
   // Rota para visualizar uma mensagem em particular:
-  @Get('/id')
-  getMessage() {}
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+  }
 }

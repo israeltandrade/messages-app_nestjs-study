cf98dae6507f23c3507f4ed9e87cac53035ed9b0
Israel Andrade DEV <israeltandrade@hotmail.com>
2022-07-31 11:05:16 -0300

    dto created and used in post validation

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,21 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
-// O decorator "Controller" é um decorator de classe.
 @Controller('messages')
 export class MessagesController {
-  // Rota para listar todas as mensagens:
-  // Aqui temos decorators de método:
   @Get()
   listMessages() {}
 
-  // Rota para listar criar uma mensagem:
-  // Decorators de argumento entram nos parâmetros da função.
+  // Passagem do objeto DTO como parâmetro de createMessage
+  // para ser usado como critério da pipe.
+  // O pacote class-transformer pega um objeto simples ou literal (referindo a
+  // classe Object do Javascript) e transforma em uma classe com construtor,
+  // propriedades e métodos próprios. (chamado tb de objetos de classe).
+  // A seguir, o corpo é transformado em uma instância da classe DTO:
   @Post()
-  createMessage(@Body() body: any) {
+  createMessage(@Body() body: CreateMessageDto) {
     console.log(body);
   }
-  // Rota para visualizar uma mensagem em particular:
   @Get('/:id')
   getMessage(@Param('id') id: string) {
     console.log(id);
